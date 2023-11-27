8d895f77536dd0b1d621bf298f60444067120803
mohammadhashemii <mohammad99hashemi@gmail.com>
2023-11-27 13:48:31 -0500

    feat: a function for saving patches added

--- utils/save_patches.py
+++ utils/save_patches.py
@@ -1,12 +1,10 @@
 import torch
 import torch.nn.functional as F
-from torchvision import transforms
 from torch.utils import data
 import SimpleITK as sitk
 import image_processing as ip
 import os
 from math import ceil
-import view3d_image
 
 def generate_IDs_list(case_list: list, n_phases=10, phases= None):
     '''
@@ -79,7 +77,60 @@ def save_patches_as_tensors(dataset_name, root, case_list: list, phases: list, e
             # view3d_image.view3d_image(patches[0, 10, :, :, :], slice_axis=0) # moving patch 
         print("{} Patches of case {} is saved!".format(patches.shape[1], str(c)))    
 
+
+def extract_and_save_patches(input_array, output_folder, patch_size=(64, 64, 64), overlap=(8, 8, 8)):
+    D, H, W = input_array.shape
+    patch_d, patch_h, patch_w = patch_size
+    overlap_d, overlap_h, overlap_w = overlap
+
+    # Create the output folder if it doesn't exist
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    patch_count = 0
+
+    for d in range(0, D, patch_d - overlap_d):
+        for h in range(0, H, patch_h - overlap_h):
+            for w in range(0, W, patch_w - overlap_w):
+                # Calculate the end indices for the current patch
+                d_end = min(d + patch_d, D)
+                h_end = min(h + patch_h, H)
+                w_end = min(w + patch_w, W)
+
+                d_start = d
+                h_start = h
+                w_start = w
+
+                if d_end - d < patch_d:
+                    d_start = d_end - patch_d
+                    d_end +=1
+
+                if h_end - h < patch_h:
+                    h_start = h_end - patch_h
+                    h_end +=1
+
+                if w_end - w < patch_w:
+                    w_start = w_end - patch_w
+                    w_end +=1
+
+                patch = input_array[d_start:d_end, h_start:h_end, w_start:w_end]
+                
+                # Save each patch to a separate file
+                patch_filename = os.path.join(output_folder, f'patch_{patch_count}.mha')
+                patch_sitk = ip.array_to_sitk(patch)
+                sitk.WriteImage(patch_sitk, patch_filename)
+                patch_count += 1
+
+                print (f'patch {patch_count} was saved at {patch_filename}')
+
+
 if __name__ == '__main__':
-    save_patches_as_tensors("DIR-Lab", root="./data/DIR-Lab/4DCT/mha_cropped/",
-                             case_list=[6, 7],
-                             phases=range(10), ext='.mha', patch_size=64, overlap_size=8)
+
+    root = "./data/DIRLAB/mha/"
+    for c in range(3, 11):
+        for p in range(10):
+            img_path = root + "case" + str(c) + "/case" + str(c) + "_T" + str(p) + "0_R.mha"
+            im_sitk = sitk.ReadImage(img_path)
+            input_array = sitk.GetArrayFromImage(im_sitk)
+            output_folder = root + "case" + str(c) + "/case" + str(c) + "_T" + str(p) + "0" +"_patches/"
+            extract_and_save_patches(input_array, output_folder, patch_size=(64, 64, 64), overlap=(8, 8, 8))

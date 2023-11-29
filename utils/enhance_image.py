67412f26a299a2a6d41160ac746b99e7773c0404
Yuxi Zheng <zhengyuxi2000@163.com>
2023-11-29 13:48:26 -0500

    Update enhance_image.py
    Change default weights from 1000 to 1500, change lower bound of range from 0.56 to 0.51.

--- utils/enhance_image.py
+++ utils/enhance_image.py
@@ -4,11 +4,11 @@ import skimage
 # input image address, write image to original image
 # the smaller the first number in the scale range
 # each entry in the mask has range[0, 1], compute as enhanced = original + mask_weight * mask
-def enhance_image(image_address, _scale_range = (0.56, 2), mask_weight = 1000): 
+def enhance_image(image_address, _scale_range = (0.51, 2), mask_weight = 2000): 
     image = sitk.ReadImage(image_address)
     image_array = sitk.GetArrayFromImage(image)
     mask = skimage.filters.frangi(image_array, black_ridges=False, beta=0.5, scale_range=_scale_range, scale_step=0.5)
-    enhanced_image_array = image_array + mask * 1000
+    enhanced_image_array = image_array + mask * mask_weight
     enhanced_image = sitk.GetImageFromArray(enhanced_image_array)
     new_address = image_address.split(".mha")[0] + "_enhanced.mha"
     sitk.WriteImage(enhanced_image, new_address)
